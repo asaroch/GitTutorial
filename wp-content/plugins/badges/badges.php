a16ec90e1c82fffd3e177581df6acc48fba00a43
ishu <ishu@Trantor-LP34>
2016-03-28 16:56:34 +0530

    Product and home page design integration

--- wp-content/plugins/badges/badges.php
+++ wp-content/plugins/badges/badges.php
@@ -27,12 +27,14 @@ class TRUST_BADGES_Widget extends WP_Widget {
 		// outputs the content of the widget
 		global $wpdb;
 		$badges = $wpdb->get_results( "SELECT id,image,external_link FROM wp_badges", ARRAY_A );
-		$badgesString = '<div class="container-fluid">
-				<div class="badges-container">';
+		$badgesString = '<div class="container">
+				<div class="badges-container">
+                                <ul>';
+                
 		foreach ( $badges as $badge ) {
-			$badgesString .= '<img src="'.$badge['image'].'" />';
+			$badgesString .= '<li><img src="'.$badge['image'].'" /></li>';
 		}
-		$badgesString .= '</div></div>';
+		$badgesString .= '</ul></div></div>';
 		echo $badgesString;
 	}
 	
