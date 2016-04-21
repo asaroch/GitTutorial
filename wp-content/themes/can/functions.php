032b360a07157a62ca9179185d6efbf6a9a2b117
anirudh.sood <anirudh.sood@Trantor29.trantorchd.com>
2016-04-21 11:41:20 +0530

    committing functions and custom js file.

--- wp-content/themes/can/functions.php
+++ wp-content/themes/can/functions.php
@@ -968,46 +968,6 @@ function resource_filter_callback() {
 
     wp_die(); // this is required to terminate immediately and return a proper response
 }
-
-/*
- * Callback function to filter the MCE settings
- */
-
-function my_mce_before_init_insert_formats($init_array) {
-
-// Define the style_formats array
-
-    $style_formats = array(
-        // Each array child is a format with it's own settings
-        array(
-            'title' => 'Top Heading',
-            'block' => 'div',
-            'classes' => 'top-heading',
-            'wrapper' => true,
-        ),
-        array(
-            'title' => 'Blue Button',
-            'block' => 'span',
-            'classes' => 'blue-button',
-            'wrapper' => true,
-        ),
-        array(
-            'title' => 'Red Button',
-            'block' => 'span',
-            'classes' => 'red-button',
-            'wrapper' => true,
-        ),
-    );
-    // Insert the array, JSON ENCODED, into 'style_formats'
-    $init_array['style_formats'] = json_encode($style_formats);
-
-    return $init_array;
-}
-
-// Attach callback to 'tiny_mce_before_init' 
-add_filter('tiny_mce_before_init', 'my_mce_before_init_insert_formats');
-
-
 /* * *********************************************
  * Adding custom widget for Member Benefits*
  * ******************************************** */
---
+++ wp-content/themes/can/small_business_funding.php
@@ -0,0 +1,277 @@
+<?php
+/*
+Template Name: small business funding
+*/
+get_header();
+?>
+<div id="sbf_hero" class="gradient-one">
+			<span class="down-arrow"></span>
+			<div class="container">
+				<div id="slider_testimonial" class="owl-carousel owl-theme">
+					<div class="item">
+						<div class="row">
+							<div class="col-md-4 col-md-push-8 col-sm-5 col-sm-push-7">
+								<div class="user-icon"> 
+								<img src="assets/images/partner/hurricane_sandy_pic.png" class="img-circle" alt="Testimonial user image">
+								</div>
+							</div>
+							<div class="col-md-8 col-md-pull-4 col-sm-7 col-sm-pull-5">
+							 	<div class="content-list-item">
+									<p>TERM LOANS</p>
+									<h3 class="heading-line">“CAN Capital made it possiblefor my business to survive Hurricane Sandy</h3>
+									<h5 class="customer-info">- – Person Name, Business Name</h5>
+									<a href="" class="btn btn-purple-style"> LEARN MORE </a>
+								</div>
+						   	</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="row">
+							<div class="col-md-4 col-md-push-8 col-sm-5 col-sm-push-7">
+								<div class="user-icon"> 
+								<img src="assets/images/partner/hurricane_sandy_pic.png" class="img-circle" alt="Testimonial user image">
+								</div>
+							</div>
+							<div class="col-md-8 col-md-pull-4 col-sm-7 col-sm-pull-5">
+							 	<div class="content-list-item">
+									<p>TERM LOANS</p>
+									<h3 class="heading-line">“CAN Capital made it possiblefor my business to survive Hurricane Sandy</h3>
+									<h5 class="customer-info">- – Person Name, Business Name</h5>
+									<a href="" class="btn btn-purple-style"> LEARN MORE </a>
+								</div>
+						   	</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="row">
+							<div class="col-md-4 col-md-push-8 col-sm-5 col-sm-push-7">
+								<div class="user-icon"> 
+								<img src="assets/images/partner/hurricane_sandy_pic.png" class="img-circle" alt="Testimonial user image">
+								</div>
+							</div>
+							<div class="col-md-8 col-md-pull-4 col-sm-7 col-sm-pull-5">
+							 	<div class="content-list-item">
+									<p>TERM LOANS</p>
+									<h3 class="heading-line">“CAN Capital made it possiblefor my business to survive Hurricane Sandy</h3>
+									<h5 class="customer-info">- – Person Name, Business Name</h5>
+									<a href="" class="btn btn-purple-style"> LEARN MORE </a>
+								</div>
+						   	</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="loan-nav">
+				<div class="container">
+					<ul>
+                                            <li class="col-xs-4 active"><a id="term_loan_btn" href="javascript:void(0)" class="navigation-item nav-right-border"> Term Loans </a></li>
+						<li class="col-xs-4"><a id="trak_loan_btn" href="javascript:void(0)" class="navigation-item nav-right-border"> TrakLoan </a></li>
+						<li class="col-xs-4"><a id="installment_btn" href="javascript:void(0)" class="navigation-item"> Installment </a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- hero -->
+		<!--Block Solution Built -->
+		<section id="solution_built">
+		<div class="fade-bg"></div>
+			<div class="container">
+				<h2 class='section-heading'>Solution build to work for you</h2>
+				<div class="divtable accordion-xs gradient-one">
+					<div class="tr headings">
+						<div class="th firstname"><span></span></div>
+						<div class="th term-laon"><span><img src="assets/images/partner/termloan_icon.png" class="img-circle" alt="Testimonial user image"><br>	 Term Loans</span></div>
+						<div class="th trak-laon"><span>TrakLoan</span></div>
+						<div class="th installment-loan"><span>Installment Loans</span></div>
+					</div>
+					<div class="tr seprate-block">
+						<div class="td firstname accordion-xs-toggle"><span>Funding Amount</span></div>
+						<div class="accordion-xs-collapse">
+							<div class="inner">
+								<div class="thead">
+									<div class="th term-laon"><span>Term Loans</span></div>
+									<div class="th trak-laon"><span>TrakLoan</span></div>
+									<div class="th installment-loan"><span>Installment Loans</span></div>
+								</div>
+								<div class="tbody">
+									<div class="td term-laon"><span>$2500-$150,000*<br><small>or more for multi-location</small></span></div>
+									<div class="td trak-laon"><span>$2500-$150,000*<br><small>or more for multi-location</small></span></div>
+									<div class="td installment-loan"><span>$50,000-$100,000*</span></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tr seprate-block">
+						<div class="td firstname accordion-xs-toggle"><span>Payments</span></div>
+						<div class="accordion-xs-collapse">
+							<div class="inner">
+								<div class="td term-laon"><span>Low Fixed Daily</span></div>
+								<div class="td trak-laon"><span>Low Flexible Daily</span></div>
+								<div class="td installment-loan"><span>Fixed Monthly</span></div>
+							</div>
+						</div>
+					</div>
+					<div class="tr seprate-block">
+						<div class="td firstname accordion-xs-toggle"><span>Length of Loan</span></div>
+						<div class="accordion-xs-collapse">
+							<div class="inner">
+								<div class="td term-laon"><span>4-36 Months</span></div>
+								<div class="td trak-laon"><span>4-36 Months**</span></div>
+								<div class="td installment-loan"><span>2,3 or 4 years</span></div>
+							</div>
+						</div>
+					</div>
+					<div class="tr seprate-block">
+						<div class="td firstname accordion-xs-toggle"><span>Owner Credit Quality</span></div>
+						<div class="accordion-xs-collapse">
+							<div class="inner">
+								<div class="td term-laon"><span>Fair to Excellent</span></div>
+								<div class="td trak-laon"><span>Fair to Excellent</span></div>
+								<div class="td installment-loan"><span>Good to Excellent</span></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="gradient-two" id="making-payments">
+			<div class="container text-center">
+				<p>Questions about <strong>applying, meeting <br/> requirements</strong> or <strong>making payments?</strong></p>
+				<a class="btn btn-purple-style" title="How it works" href="javascript:void(0);">How it works</a>
+			</div>
+		</section>
+		<!--Right Financial-->
+		<section id="right-financing">
+			<div class="container">
+				<h2 class="section-heading">Your greatest potential begin with<br> the right financing</h2>
+				<div id="infografic_carousel" class="owl-carousel owl-theme">
+					<div class="item">
+						<div class="info-product-item">							
+							<div class="graph1"><img src="assets/images/partner/garph-1.png" alt="installation icon image" class="img-responsive"></div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="graph1"><img src="assets/images/partner/garph-2.png" alt="installation icon image" class="img-responsive"></div>
+					</div>
+					<div class="item">
+						<div class="graph1"><img src="assets/images/partner/garph-1.png" alt="installation icon image" class="img-responsive"></div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Right Financial -->
+		<!--funding option-->
+		<section id="funding-option">
+			<div class="container">
+				<h2 class="section-heading">Experience a better funding option</h2>
+				<div class="divtable accordion-xs gradient-one">
+					<div class="tr headings">
+						<div class="th firstname"><span></span></div>
+						<div class="th term-laon"><span><img alt="" src="assets/images/home/CAN_logo_footer.png" width="140" height="20"></span></div>
+						<div class="th trak-laon"><span>Bank Loan</span></div>
+						<div class="th installment-loan"><span>Credit card</span></div>
+					</div>
+					<div class="tr seprate-block">
+						<div class="td firstname accordion-xs-toggle"><span>Expert in small business</span></div>
+						<div class="accordion-xs-collapse" aria-expanded="false">
+							<div class="inner">
+								<div class="thead">
+									<div class="th term-laon"><span><img alt="" src="assets/images/CAN-logo.png" class="img-responsive" width="130" height="25"></span></div>
+									<div class="th trak-laon"><span>TrakLoan</span></div>
+									<div class="th installment-loan"><span>Installment Loans</span></div>
+								</div>
+								<div class="tbody">
+									<div class="td term-laon"><span><img src="assets/images/termsloan/check_bullet.png" alt="Check"/><span></div>
+									<div class="td trak-laon"></div>
+									<div class="td installment-loan"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tr seprate-block">
+						<div class="td firstname accordion-xs-toggle"><span>Fund Sent in As Littel As 2 Days'</span></div>
+						<div class="accordion-xs-collapse" aria-expanded="false">
+							<div class="inner">
+								<div class="td term-laon"><span><img src="assets/images/termsloan/check_bullet.png" alt="Check"/></span></div>
+								<div class="td trak-laon"></div>
+								<div class="td installment-loan"></div>
+							</div>
+						</div>
+					</div>
+					<div class="tr seprate-block">
+						<div class="td firstname accordion-xs-toggle"><span>Minmal Paperwork</span></div>
+						<div class="accordion-xs-collapse" aria-expanded="false">
+							<div class="inner">
+								<div class="td term-laon"><span><img src="assets/images/termsloan/check_bullet.png" alt="Check"/></span></div>
+								<div class="td trak-laon"></div>
+								<div class="td installment-loan"><span><img src="assets/images/termsloan/check_bullet.png" alt="Check"/></span></div>
+							</div>
+						</div>
+					</div>
+					<div class="tr seprate-block">
+						<div class="td firstname accordion-xs-toggle"><span>Consider More Than Owner's Credit Score</span></div>
+						<div class="accordion-xs-collapse" aria-expanded="false">
+							<div class="inner">
+								<div class="td term-laon"><span><img src="assets/images/termsloan/check_bullet.png" alt="Check"/></span></div>
+								<div class="td trak-laon"><span><img src="assets/images/termsloan/check_bullet.png" alt="Check"/></span></div>
+								<div class="td installment-loan"></div>
+							</div>
+						</div>
+					</div>
+					<div class="tr seprate-block">
+						<div class="td firstname accordion-xs-toggle"><span>Multiple Payment Option</span></div>
+						<div class="accordion-xs-collapse" aria-expanded="false">
+							<div class="inner">
+								<div class="td term-laon"><span><img src="assets/images/termsloan/check_bullet.png" alt="Check"/></span></div>
+								<div class="td trak-laon"></div>
+								<div class="td installment-loan"></div>
+							</div>
+						</div>
+					</div>
+					<div class="tr seprate-block">
+						<div class="td firstname accordion-xs-toggle"><span>Repeat Customer Benefits</span></div>
+						<div class="accordion-xs-collapse" aria-expanded="false">
+							<div class="inner">
+								<div class="td term-laon"><span><img src="assets/images/termsloan/check_bullet.png" alt="Check"/></span></div>
+								<div class="td trak-laon"></div>
+								<div class="td installment-loan"><span><img src="assets/images/termsloan/check_bullet.png" alt="Check"/></span></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--funding option-->
+	<!-- CTA -->	
+		<section id="cta_block">
+			<div class="container">
+				<div class="col-md-12">
+					<div class="row">
+						<h2 class="section-heading"> Industry Recognition </h2>
+					</div>
+				</div>
+				<div class="col-md-12">
+					<div class="row">
+						<ul class="partners-list">
+							<li class="col-md-3 col-sM-3"> <a href="#"> <img alt="" src="assets/images/partner/forbs.png"> </a> </li>
+							<li class="col-md-3 col-sm-3"> <a href="#"> <img alt="" src="assets/images/partner/inc.png"> </a> </li>
+							<li class="col-md-3 col-sm-3"> <a href="#"> <img alt="" src="assets/images/partner/b_insider.png"> </a> </li>
+							<li class="col-md-3 col-sm-3"> <a href="#"> <img alt="" src="assets/images/partner/bbb_ratting.png"> </a> </li>
+						</ul>
+					</div>
+				</div>
+			</div>		
+		</section>
+		<!-- CTA -->
+		<!-- Get Funded -->
+		<section  class="get-funded">
+			<div class="container text-center">
+				<h2 class="section-heading"> Get Funded </h2>
+				<h3> Smart, Simple & Fast. </h3>
+				<a href="javascript:void(0);" title="APPLY NOW" class="btn btn-blue-bg"> APPLY NOW <i class="glyphicon glyphicon-play"></i></a>
+			</div>
+		</section>
+		<!-- Get Funded -->
+
+<?php get_footer(); ?>
+
