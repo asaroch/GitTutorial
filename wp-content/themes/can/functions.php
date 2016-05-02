fafef8c666be036c1e73224f1f989d9935cfd357
ishu <ishu@Trantor-LP34>
2016-05-02 11:09:28 +0530

    Instant Quote section

--- wp-content/themes/can/functions.php
+++ wp-content/themes/can/functions.php
@@ -363,6 +363,8 @@ function can_scripts() {
     
     // Fetch Quick Quote validation error messages
     $quickQuotevalidationsErrs = get_option('quick_quote_generations_validations_error_msg');
+    //Fetch quick quote field option values
+    $fieldOptionValue = get_option('quick_quote_field_options');
     
     // Search parameters of resource
     $resourceFilteredParameters = array();
@@ -378,7 +380,8 @@ function can_scripts() {
         'testimonialSlider'      => $testimonialSlider,
         'validationsErrs'        => $validationsErrs,
         'resourceFilteredParameters'  => $resourceFilteredParameters,
-        'quickQuotevalidationsErrs' => $quickQuotevalidationsErrs
+        'quickQuotevalidationsErrs' => $quickQuotevalidationsErrs,
+        'fieldOptionValue' => $fieldOptionValue
       ));
 }
 
@@ -1647,3 +1650,35 @@ function resource_detail_copy_text() {
     return $return;
 }
 add_shortcode( 'resource-detail-copy-text', 'resource_detail_copy_text' );
+
+/* * ****************************************************************************
+ * Function to generate thumbnail of a video
+ * ********************************************************* *********************/
+function video_thumbnail( $video , $size = '1144x493', $post) {
+     if ( $video != '') {
+        // Script to generate thumbnail from video* */
+      $ffmpeg = 'ffmpeg';
+
+      // where you'll save the image
+      $upload_url = wp_upload_dir();
+      $image = $upload_url['basedir'] . "/thumbnails/" . $post->ID . ".jpg";
+
+      // default time to get the image
+      $second = 1;
+
+      // get the duration and a random place within that
+      $cmd = "$ffmpeg -i $video 2>&1";
+      if (preg_match('/Duration: ((\d+):(\d+):(\d+))/s', `$cmd`, $time)) {
+          $total = ($time[2] * 3600) + ($time[3] * 60) + $time[4];
+          $second = rand(1, ($total - 1));
+      }
+
+      // get the screenshot
+      $cmd = "$ffmpeg -i $video -deinterlace -an -ss $second -t 00:00:01 -r 1 -y -s $size -vcodec mjpeg -f mjpeg $image 2>&1";
+      $return = `$cmd`;
+      //Script Ends here* */
+      $src = $upload_url['baseurl'] . "/thumbnails/" . $post->ID . ".jpg";
+      return $src;
+    }
+    
+}
