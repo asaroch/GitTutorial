1fdb00fb2a5dd7169c9ede7d03d0703a27161a4c
anirudh.sood <anirudh.sood@Trantor29.trantorchd.com>
2016-05-09 12:58:48 +0530

    changing head title 60 limit JS code.

--- wp-content/themes/can/functions.php
+++ wp-content/themes/can/functions.php
@@ -1982,6 +1982,8 @@ function ajax_glossary_pagination() {
 function title_count_js() {
 
         echo '<script>jQuery(document).ready(function(){
+            if(typeof jQuery("#wp-wpcf-page-headline-title-editor-container").html() != "undefined"){
+                jQuery("#wp-wpcf-page-headline-title-editor-container").
 		jQuery("#wp-wpcf-page-headline-title-editor-container #qt_wpcf-page-headline-title_toolbar").after("<div style=\"position:relative;color:#666;\"><small>Title length: </small><span id=\"title_counter\"></span><span style=\"font-weight:bold; padding-left:7px;\">/ 60</span><small><span style=\"font-weight:bold; padding-left:7px;\">character(s).</span></small></div>");
 			 jQuery("span#title_counter").text(jQuery("#wpcf-page-headline-title").val().length);
 			 jQuery("#wpcf-page-headline-title").keyup( function() {
@@ -1990,6 +1992,7 @@ function title_count_js() {
     }
 			 jQuery("span#title_counter").text(jQuery("#wpcf-page-headline-title").val().length);
 		   });
+                }  // run code if defined else not.
 		});</script>';
 }
 
