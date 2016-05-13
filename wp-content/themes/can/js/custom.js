c6fadbb37e5e5fca986effde329d5a4c017f380f
manpreet.kaur <manpreet.kaur@TRANTOR173.trantorchd.com>
2016-05-13 11:48:36 +0530

    merged custom SBF slider

--- wp-content/themes/can/js/custom.js
+++ wp-content/themes/can/js/custom.js
@@ -410,42 +410,46 @@ $(function () {
                     }
 
         });
-        
-        /* Small business funding slider */
-                var sbfSlider = $("#sbf_hero #slider_testimonial");
-                $("#installment_btn").click(function () {
-                    sbfSlider.trigger("to.owl.carousel", [2, 500, true]);
-                });
-                $("#trak_loan_btn").click(function () {
-                    sbfSlider.trigger("to.owl.carousel", [1, 500, true]);
-                });
-                $("#term_loan_btn").click(function () {
-                    sbfSlider.trigger("to.owl.carousel", [0, 500, true]);
-                });
-                sbfSlider.on('changed.owl.carousel', function(event) {
-                    console.log('in changed event', event);
-                    switch(event.item.index) {
-                        case 0:
-                            var anchorParent = $("#term_loan_btn").parent();
-                            break;
-                        case 1:
-                            var anchorParent = $("#trak_loan_btn").parent();
-                            break;
-                        case 2:
-                            var anchorParent = $("#installment_btn").parent();
-                    }
-                    console.log(anchorParent);
-                    var parentSiblings = anchorParent.siblings("li.active");
-                    parentSiblings.removeClass("active");
-                    anchorParent.addClass("active");
-                });    
-                $(".navigation-item").click(function () {
-                    var $this = $(this);
-                    var anchorParent = $this.parent();
-                    var parentSiblings = anchorParent.siblings("li.active");
-                    parentSiblings.removeClass("active");
-                    anchorParent.addClass("active");
-                });
+    /* Small business funding slider */
+            var sbfSlider = $("#sbf_hero #slider_testimonial");
+            $("#installment_btn").click(function () {
+                sbfSlider.trigger("to.owl.carousel", [2, 500, true]);
+            });
+            $("#trak_loan_btn").click(function () {
+                sbfSlider.trigger("to.owl.carousel", [1, 500, true]);
+            });
+            $("#term_loan_btn").click(function () {
+                sbfSlider.trigger("to.owl.carousel", [0, 500, true]);
+            });
+            sbfSlider.on('changed.owl.carousel', function(event) {
+                var itemIndex = event.item.index;
+                if(itemIndex >= 3){
+                    itemIndex += 1
+                }
+                switch(itemIndex % 3) {
+                    case 0:
+                        var anchorParent = $("#term_loan_btn").parent();
+                        break;
+                    case 1:
+                        var anchorParent = $("#trak_loan_btn").parent();
+                        break;
+                    case 2:
+                        var anchorParent = $("#installment_btn").parent();
+                }
+                console.log(anchorParent);
+                var parentSiblings = anchorParent.siblings("li.active");
+                parentSiblings.removeClass("active");
+                anchorParent.addClass("active");
+            });
+            /*active state of slider buttons*/
+            $(".navigation-item").click(function () {
+                var $this = $(this);
+                var anchorParent = $this.parent();
+                var parentSiblings = anchorParent.siblings("li.active");
+                parentSiblings.removeClass("active");
+                anchorParent.addClass("active");
+                sbfSlider.trigger("stop.owl.autoplay");
+            });
     /* Small business funding slider state */
             
             // clear all - search resource
